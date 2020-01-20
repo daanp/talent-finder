0558dcb70f1834d71d8965f2e0f682f8677c3a25
daanp <daan24@gmail.com>
2020-01-20 15:50:46 +0100

    add candidate POST

--- routes/candidates.js
+++ routes/candidates.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+var candidates = [];
+
+/* GET candidates listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(candidates);
 });
+
+router.post('/', function(req, res, next) {
+  candidates.push(req.body);
+  res.sendStatus(200);
+})
+
+
+router.post('/')
 
 module.exports = router;
